f3626c543757c77e07b1f27442a66b28dc0e79f2
Rutika72 <103304552+Rutika72@users.noreply.github.com>
2026-01-15 01:02:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const modal = document.querySelector('.modal');
+const openBtn = document.getElementById('openModal');
+const closeBtn = document.querySelector('.close-modal');
+
+// Open modal when button clicked
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+// Close modal when close button clicked
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Close modal when clicking outside modal-content
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
